f6db16b26c8594e2edb19dc8569e6f7e96c14b85
nishant <nyindorkar@gmail.com>
2023-04-25 10:52:05 +0530

    let see

--- deployment.groovy
+++ deployment.groovy
@@ -6,16 +6,17 @@ pipeline {
             steps {
                 sh 'sudo apt update -y'
                 git credentialsId: 'jenkins', url: 'git@github.com:nishantindorkar/student-ui.git'
+                sh 'pwd'
                 sh 'ls'
             }
         }
-        stage("build-gradle") {
-            steps { 
-                sh 'sudo apt update -y'
-                sh 'sudo apt-get install gradle curl unzip -y'
-                sh 'gradle clean build'
-            }
-        } 
+        // stage("build-gradle") {
+        //     steps { 
+        //         sh 'sudo apt update -y'
+        //         sh 'sudo apt-get install gradle curl unzip -y'
+        //         sh 'gradle clean build'
+        //     }
+        // } 
         // stage('SonarQube Analysis') {
         //     steps {
         //         withSonarQubeEnv('sonarqube-sonar') {
