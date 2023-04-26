04d27ccf9287021bdb3cbdf02f153618025d8fc9
nishant <nyindorkar@gmail.com>
2023-04-26 11:27:02 +0530

    docker install

--- deployment.groovy
+++ deployment.groovy
@@ -39,6 +39,20 @@ pipeline {
                 sh 'aws s3 cp **/*.war s3://artifact-studentui/student-${BUILD_ID}.war'
             }
         }
+        stage('Docker Build') {
+            steps{
+                sh '''
+                sudo apt update -y
+                sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+                echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt update -y
+                sudo apt install docker-ce docker-ce-cli containerd.io -y
+                docker --version
+                echo "successfully installed"
+                '''
+            }
+        }
         stage('Post-build Cleanup') {
             steps {
                 //sh 'mvn clean'
